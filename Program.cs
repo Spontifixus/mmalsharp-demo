28590a21afbdb226de6bdaf97ef61b88c5e3df93
Markus Rehrs <markus.rehrs@zuehlke.com>
2020-04-11 22:26:30 +0200

    Added example for camera configuration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using MMALSharp;
@@ -6,12 +7,21 @@ using MMALSharp.Common;
 using MMALSharp.Common.Utility;
 using MMALSharp.Components;
 using MMALSharp.Handlers;
+using MMALSharp.Native;
 using MMALSharp.Ports;
 
 namespace BufferDemo
 {
     class Program
     {
+        private static ILogger<Program> log;
+        private static MMALImageEncoder imageEncoder;
+        private static MMALResizerComponent resizer;
+        private static IMMALPortConfig encoderPortConfig;
+        private static MMALPortConfig resizerPortConfig;
+        private static MMALNullSinkComponent nullSink;
+        private static MemoryStreamCaptureHandler memoryStreamCaptureHandler;
+
         static async Task Main()
         {
             var loggerFactory = LoggerFactory.Create(builder =>
@@ -19,68 +29,115 @@ namespace BufferDemo
                 builder.AddConsole(o => o.DisableColors = true);
                 builder.SetMinimumLevel(LogLevel.Debug);
             });
-            var log = loggerFactory.CreateLogger<Program>();
-            MMALLog.LoggerFactory = loggerFactory;
-
-            log.LogInformation("Configuring camera...");
-
-            var camera = MMALCamera.Instance;
-            camera.ConfigureCameraSettings();
-
-            var imageEncoder = new MMALImageEncoder();
-            var resizer = new MMALResizerComponent();
-
-            var encoderPortConfig = new MMALPortConfig(MMALEncoding.JPEG, MMALEncoding.I420, 90);
-            var resizerPortConfig = new MMALPortConfig(MMALEncoding.I420, MMALEncoding.I420, 1024, 768, 0, 0, 0, false, null);
-
-            resizer.ConfigureInputPort(new MMALPortConfig(MMALEncoding.OPAQUE, MMALEncoding.I420), camera.Camera.StillPort,
-                    null)
-                .ConfigureOutputPort(resizerPortConfig, null);
-
-            var nullSink = new MMALNullSinkComponent();
-
-            camera.Camera.StillPort.ConnectTo(resizer);
-            resizer.Outputs[0].ConnectTo(imageEncoder);
-            camera.Camera.PreviewPort.ConnectTo(nullSink);
-            log.LogInformation("Camera configuration completed.");
-
-            for (var i = 0; i < 100; i++)
+            log = loggerFactory.CreateLogger<Program>();
+            try
             {
-                // Create a memory stream handler, so the image
-                // data that was captured gets stored in a stream
-                // for further handling.
-                using var memoryStreamHandler = new MemoryStreamCaptureHandler();
+                MMALCameraConfig.Debug = true;
+                MMALLog.LoggerFactory = loggerFactory;
 
-                // Capture th image
-                log.LogInformation("Capturing image...");
-                imageEncoder.ConfigureOutputPort(encoderPortConfig, memoryStreamHandler);
-                await camera.ProcessAsync(camera.Camera.StillPort);
-
-                // Sometimes the stream is empty after capturing.
-                // If that happens, we skip the storing the image
-                // and continue by capturing the next image
-                if (memoryStreamHandler.CurrentStream.Length == 0)
-                {
-                    log.LogWarning("Capturing image failed. Skipping iteration.");
-                    continue;
-                }
-                log.LogInformation("Image captured successfully.");
+                log.LogInformation("Capturing first image...");
+                PreparePipeline();
+                MMALCameraConfig.Rotation = 180;
+                MMALCamera.Instance.ConfigureCameraSettings();
+                await CaptureImage(
+                    1); // Usually this would run in a loop and capture a bunch of images with the same config.
+                log.LogInformation("Captured first image.");
 
 
-                // Simulate Storage
-                // In the real world code this gets uploaded
-                // to an Azure blob storage - that's why I did
-                // not use one of the other capture handlers here.
-                log.LogInformation("Storing image...");
-                if (!Directory.Exists("images"))
-                    Directory.CreateDirectory("images");
-                await using var fileStream = File.OpenWrite($"images/test{i}.jpg");
-                memoryStreamHandler.CurrentStream.Seek(0, SeekOrigin.Begin);
-                await memoryStreamHandler.CurrentStream.CopyToAsync(fileStream);
-                log.LogInformation("Image stored successfully...");
+                log.LogInformation("Capturing second image...");
+                PreparePipeline();
+                MMALCameraConfig.Rotation = 180;
+                MMALCameraConfig.ISO = 800;
+                MMALCameraConfig.ShutterSpeed = 2000 * 1000;
+                MMALCamera.Instance.ConfigureCameraSettings();
+                await CaptureImage(
+                    2); // Usually this would run in a loop and capture a bunch of images with the same config.
+                log.LogInformation("Captured second image.");
 
-                memoryStreamHandler.Dispose();
+                log.LogInformation("Capturing third image...");
+                PreparePipeline();
+                MMALCameraConfig.Rotation = 180;
+                MMALCameraConfig.ExposureMode = MMAL_PARAM_EXPOSUREMODE_T.MMAL_PARAM_EXPOSUREMODE_AUTO;
+                MMALCamera.Instance.ConfigureCameraSettings();
+                await CaptureImage(
+                    3); // Usually this would run in a loop and capture a bunch of images with the same config.
+                log.LogInformation("Captured third image.");
             }
+            catch (Exception ex)
+            {
+                log.LogError(ex, "Here goes the exception!");
+            }
+        }
+
+        private static async Task CaptureImage(int index)
+        {
+            // Create a memory stream handler, so the image
+            // data that was captured gets stored in a stream
+            // for further handling.
+
+            // Capture th image
+            log.LogInformation("Capturing image...");
+            await MMALCamera.Instance.ProcessAsync(MMALCamera.Instance.Camera.StillPort);
+
+            // Sometimes the stream is empty after capturing.
+            // If that happens, we skip the storing the image
+            // and continue by capturing the next image
+            if (memoryStreamCaptureHandler.CurrentStream.Length == 0)
+            {
+                log.LogWarning("Capturing image failed. Skipping iteration.");
+                return;
+            }
+            log.LogInformation("Image captured successfully.");
+
+            // Simulate Storage
+            // In the real world code this gets uploaded
+            // to an Azure blob storage - that's why I did
+            // not use one of the other capture handlers here.
+            log.LogInformation("Storing image...");
+            if (!Directory.Exists("images"))
+                Directory.CreateDirectory("images");
+            await using var fileStream = File.OpenWrite($"images/test{index}.jpg");
+            
+            memoryStreamCaptureHandler.CurrentStream.Seek(0, SeekOrigin.Begin);
+            await memoryStreamCaptureHandler.CurrentStream.CopyToAsync(fileStream);
+
+            memoryStreamCaptureHandler.CurrentStream.Seek(0, SeekOrigin.Begin);
+            memoryStreamCaptureHandler.CurrentStream.SetLength(0);
+
+            log.LogInformation("Image stored successfully...");
+        }
+
+        private static void PreparePipeline()
+        {
+            log.LogInformation("Preparing camera pipeline...");
+
+            log.LogDebug("Disposing of old parts...");
+            imageEncoder?.Dispose();
+            resizer?.Dispose();
+            nullSink?.Dispose();
+            memoryStreamCaptureHandler?.Dispose();
+
+            log.LogDebug("Creating and configuring pipeline parts...");
+            
+            memoryStreamCaptureHandler = new MemoryStreamCaptureHandler();
+
+            imageEncoder = new MMALImageEncoder();
+            resizer = new MMALResizerComponent();
+
+            encoderPortConfig = new MMALPortConfig(MMALEncoding.JPEG, MMALEncoding.I420, 90);
+            resizerPortConfig = new MMALPortConfig(MMALEncoding.I420, MMALEncoding.I420, 1024, 768, 0, 0, 0, false, null);
+
+            imageEncoder.ConfigureOutputPort(encoderPortConfig, memoryStreamCaptureHandler);
+            resizer.ConfigureInputPort(new MMALPortConfig(MMALEncoding.OPAQUE, MMALEncoding.I420), MMALCamera.Instance.Camera.StillPort, null)
+                   .ConfigureOutputPort(resizerPortConfig, null);
+
+            nullSink = new MMALNullSinkComponent();
+
+            MMALCamera.Instance.Camera.StillPort.ConnectTo(resizer);
+            resizer.Outputs[0].ConnectTo(imageEncoder);
+            MMALCamera.Instance.Camera.PreviewPort.ConnectTo(nullSink);
+
+            log.LogInformation("Pipeline preparation completed.");
         }
     }
 }
